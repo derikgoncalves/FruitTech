4665d8f65ea9c4855b9fe95d9ce49be623e7d7a2
Isaac SS Junior <142359555+IsaacSSJunior@users.noreply.github.com>
2023-09-30 20:37:41 -0300

    Add files via upload

--- Código Fonte/Banco de Dados/SQLDatabase_Fruittech.sql
+++ Código Fonte/Banco de Dados/SQLDatabase_Fruittech.sql
@@ -1,93 +1,93 @@
-create database FruitTech;
+USE FruitTech;
 
-use FruitTech;
-
-create table agricultor(
-idAgricultor int primary key auto_increment,
-nome varchar(50) not null,
-cpf char(11) unique not null,
-telefone varchar(15) unique not null,
-fazenda varchar(65) not null,
-cep char(9) not null,
-uf char(2) not null
+CREATE TABLE tbAgricultor (
+  idAgricultor INT PRIMARY KEY AUTO_INCREMENT,
+  nomeCompletoAgricultor VARCHAR(50) NOT NULL,
+  cpfAgricultor CHAR(11) UNIQUE NOT NULL,
+  cnpjAgricultor CHAR(14) UNIQUE DEFAULT 'Nenhum CNPJ.',
+  senhaAgricultor VARCHAR(40) NOT NULL,
+  nomeFazendaAgricultor VARCHAR(65) DEFAULT 'Nenhum nome de fazenda.',
+  logradouroAgricultor VARCHAR(100) NOT NULL,
+  numLogradouroAgricultor INT NOT NULL,
+  complementoAgricultor VARCHAR(40) DEFAULT 'Nenhum complemento.',
+  bairroAgricultor VARCHAR(60) NOT NULL,
+  cidadeAgricultor VARCHAR(40) NOT NULL,
+  estadoAgricultor VARCHAR(40) NOT NULL,
+  cepAgricultor CHAR(8) UNIQUE NOT NULL
 );
 
-create table transportadora(
-idTransportadora int primary key auto_increment,
-nomeTransp varchar(65) not null,
-telefoneTransp varchar(15) unique not null, 
-cnpj char(14) unique not null,
-cep char(9) not null,
-uf char(2) not null
+CREATE TABLE tbFoneAgricultor (
+  idfoneAgricultor INT PRIMARY KEY AUTO_INCREMENT,
+  numTelefoneAgricultor CHAR(13) NOT NULL,
+  fkAgricultor INT,
+  CONSTRAINT fkAgric FOREIGN KEY (fkAgricultor) REFERENCES tbAgricultor (idAgricultor)
 );
 
-create table caminhao(
-placa char(7) primary key,
-comprimento decimal(5, 2),
-altura decimal(4, 2),
-statusMonitoramento varchar(7),
-fkAgricultor int,
-constraint fkAgri foreign key (fkAgricultor) references agricultor(idAgricultor),
-fkTransportadora int,
-constraint fkTransp foreign key (fkTransportadora) references transportadora(idTransportadora), 
-constraint chkStatusMonitoramento check (statusMonitoramento in ('Ativo', 'Inativo'))
-);
-        
-create table alerta (
-idAlerta int primary key auto_increment,   
-tipoAlerta varchar(20),
-descricaoAlerta text,
-acoesTomadas text,
-Timestamp timestamp
+CREATE TABLE tbCaminhao (
+  placaCaminhao CHAR(7) PRIMARY KEY UNIQUE NOT NULL,
+  marcaCaminhao VARCHAR(40) NOT NULL,
+  modeloCaminhao VARCHAR(40) NOT NULL,
+  anoFabricacaoCaminhao CHAR(4) NOT NULL,
+  chassiCaminhao CHAR(17) UNIQUE NOT NULL,
+  capacidadeCaminhao DECIMAL(4, 1) NOT NULL,
+  comprimentoCaminhao DECIMAL(5, 2) NOT NULL,
+  alturaCaminhao DECIMAL(4, 2) NOT NULL,
+  odometroCaminhao INT NOT NULL,
+  tipoCombustivelCaminhao VARCHAR(10) NOT NULL,
+  CONSTRAINT chkTipoCombustivel CHECK (tipoCombustivelCaminhao IN ('Diesel Comum', 'Diesel S-10', 'Diesel aditivado', 'Diesel premium', 'Gás Natural', 'Eletricidade', 'Outro'))
 );
 
-create table produto (
-idProduto int primary key auto_increment,
-tipoProduto varchar(9),
-nomeProduto varchar(80),
-tempIdeal decimal(5, 2),
-umidIdeal decimal(5, 2),
-volumePlantio decimal(10, 2),
-dataPlantio date,
-condicaoAtual varchar(7),
-constraint chkTipoProduto check (tipoProduto  in ('Fruta', 'Hortaliça')),
-constraint chkCondicaoAtual check (condicaoAtual in ('Bom', 'Alerta', 'Crítico'))
+CREATE TABLE Produto (
+  idProduto INT PRIMARY KEY AUTO_INCREMENT,
+  tipoProduto VARCHAR(10) NOT NULL,
+  nomeProduto VARCHAR(80) NOT NULL,
+  tempIdeal DECIMAL(5, 2) NOT NULL,
+  umidIdeal DECIMAL(5, 2) NOT NULL,
+  CONSTRAINT chkTipoProduto CHECK (tipoProduto IN ('Fruta')),
+  fkAgricultor INT,
+  CONSTRAINT fkAgric FOREIGN KEY (fkAgricultor) REFERENCES tbAgricultor (idAgricultor)
+) AUTO_INCREMENT = 10;
+
+CREATE TABLE Remessa (
+  idRemessa INT PRIMARY KEY AUTO_INCREMENT,
+  quantidade INT NOT NULL,
+  dataEnvio DATE NOT NULL,
+  dataRecebimento DATE NOT NULL,
+  LogradourolocalOrigem VARCHAR(80) NOT NULL,
+  numLogradouroLO INT NOT NULL,
+  complementoLO VARCHAR(40) NOT NULL,
+  bairroLO VARCHAR(40) NOT NULL,
+  cidadeLO VARCHAR(40) NOT NULL,
+  estadoLO VARCHAR(40) NOT NULL,
+  cepLO CHAR(8) UNIQUE NOT NULL,
+  LogradourolocalDestino VARCHAR(80) NOT NULL,
+  numLogradouroLD INT NOT NULL,
+  complementoLD VARCHAR(40),
+  bairroLD VARCHAR(40) NOT NULL,
+  cidadeLD VARCHAR(40) NOT NULL,
+  estadoLD VARCHAR(40) NOT NULL,
+  cepLD CHAR(8) UNIQUE NOT NULL,
+  NCM VARCHAR(10) NOT NULL,
+  distancia DOUBLE NOT NULL,
+  fkProduto INT,
+  CONSTRAINT fkProdut FOREIGN KEY (fkProduto) REFERENCES Produto (idProduto),
+  fkCaminhao CHAR(7),
+  CONSTRAINT fkCaminh FOREIGN KEY (fkCaminhao) REFERENCES tbCaminhao (placaCaminhao)
+) AUTO_INCREMENT = 100;
+
+CREATE TABLE Sensor (
+  idSensor INT PRIMARY KEY AUTO_INCREMENT,
+  tipo VARCHAR(5) NOT NULL,
+  statusMonitoramento VARCHAR(7) NOT NULL,
+  CONSTRAINT chktipo CHECK (tipo IN ('DHT11', 'LM35')),
+  CONSTRAINT chkStatusMonitoramento CHECK (statusMonitoramento IN ('Ativo', 'Inativo')),
+  fkCaminhao CHAR(7),
+  CONSTRAINT fkCaminh FOREIGN KEY (fkCaminhao) REFERENCES tbCaminhao (placaCaminhao)
 );
-  
--- INSERTS
-insert into agricultor (nome, cpf, telefone, fazenda, cep, uf) values
-('João Silva', '12345678901', '11987654321', 'Fazenda do choro', '09876543', 'SP'),
-('Maria Santos', '23456789012', '21987654321', 'Fazenda da Alegria', '08765432', 'RJ'),
-('Pedro Oliveira', '34567890123', '31987654321', 'Fazenda Rei Do Sol', '07654321', 'MG');
 
-insert into transportadora (nomeTransp, telefoneTransp, cnpj, cep, uf) values
-('DHL', '11987654321', '12345678901234', '09876543', 'SP'),
-('Lalamove', '21987654321', '23456789012345', '08765432', 'RJ'),
-('AgroTransp', '31987654321', '34567890123456', '07654321', 'MG');
-
-insert into caminhao (placa, comprimento, altura, statusMonitoramento, fkAgricultor, fkTransportadora) values
-('ABC1234', 10.5, 4.2, 'Ativo', 1, null),
-('DEF5678', 12.3, 4.5, 'Ativo', 2, null),
-('GHI9012', 11.2, 4.3, 'Ativo', null, 3),
-('JKL0123', 15.0, 5.0, 'Ativo', 2, null),
-('MNO4567', 6.0, 4.0, 'Inativo', null, 3),
-('PQR5678', 28.8, 5.0, 'Ativo', null, 1),
-('STU6789', 17.1, 4.5, 'Ativo', null, 2),
-('VWX7890', 12.0, 4.7, 'Ativo', null, 1),
-('YZA8901', 15.9, 4.5, 'Ativo', null, 2);
-
-insert into alerta (TipoAlerta ,DescricaoAlerta ,AcoesTomadas ,Timestamp ) values
-('Temperatura','Temperatura acima do ideal para o produto','Abaixar temperatura local','2023-09-09 23:43:07'),
-('Umidade','Umidade abaixo do ideal para o produto','Aumentar umidade local','2023-09-09 23:43:07'),
-('Umidade','Umidade acima do ideal para o produto','Abaixar umidade local','2023-09-09 23:43:07');
-
-insert into Produto (tipoProduto, nomeProduto, tempIdeal, umidIdeal, volumePlantio, dataPlantio, condicaoAtual) values
-('Fruta', 'Maçã Gala', 20.5, 60.0, 1000.0, '2023-09-01','Bom'),
-('Hortaliça','Alface Crespa' ,18.0 ,70.0 ,500.0 ,'2023-09-11','Alerta'),
-('Fruta','Banana Prata' ,22.0 ,65.0 ,1500.0 ,'2023-09-17','Crítico');
-
--- SELECTS
-select nome, fazenda, placa, statusMonitoramento from agricultor join caminhao on fkAgricultor = idAgricultor;
-select nomeTransp, placa, statusMonitoramento from transportadora join caminhao on fkTransportadora = idTransportadora;
-select DescricaoAlerta ,AcoesTomadas, Timestamp from alerta;
-select nomeProduto, dataPlantio, condicaoAtual from produto;
+CREATE TABLE sensorDados (
+  id_sensorDados INT PRIMARY KEY AUTO_INCREMENT,
+  umidade DOUBLE NOT NULL,
+  temperatura DOUBLE NOT NULL,
+  data_Hora DATETIME DEFAULT CURRENT_TIMESTAMP
+);
