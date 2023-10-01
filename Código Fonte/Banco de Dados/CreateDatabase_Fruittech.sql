83b7e4546a4556924ba219c62006a94423c8d61d
Isaac <Isaac.silvajr@sptech.school>
2023-10-01 16:25:29 -0300

    Create finalizado, Insert (em progresso)

--- Código Fonte/Banco de Dados/CreateDatabase_Fruittech.sql
+++ Código Fonte/Banco de Dados/CreateDatabase_Fruittech.sql
@@ -1,26 +1,29 @@
+CREATE DATABASE FruitTech;
 USE FruitTech;
+
 
 CREATE TABLE tbAgricultor (
   idAgricultor INT PRIMARY KEY AUTO_INCREMENT,
-  nomeCompletoAgricultor VARCHAR(50) NOT NULL,
-  cpfAgricultor CHAR(11) UNIQUE NOT NULL,
-  cnpjAgricultor CHAR(14) UNIQUE DEFAULT 'Nenhum CNPJ.',
-  senhaAgricultor VARCHAR(40) NOT NULL,
+  nomeAgricultor VARCHAR(50) NOT NULL,
+  sobrenomeAgricultor VARCHAR(50) NOT NULL,
+  cpfAgricultor CHAR(14) UNIQUE NOT NULL,
+  cnpjAgricultor CHAR(18) UNIQUE DEFAULT 'Nenhum CNPJ.',
+  senhaAgricultor VARCHAR(45) NOT NULL,
   nomeFazendaAgricultor VARCHAR(65) DEFAULT 'Nenhum nome de fazenda.',
   logradouroAgricultor VARCHAR(100) NOT NULL,
   numLogradouroAgricultor INT NOT NULL,
-  complementoAgricultor VARCHAR(40) DEFAULT 'Nenhum complemento.',
-  bairroAgricultor VARCHAR(60) NOT NULL,
-  cidadeAgricultor VARCHAR(40) NOT NULL,
-  estadoAgricultor VARCHAR(40) NOT NULL,
-  cepAgricultor CHAR(8) UNIQUE NOT NULL
+  complementoAgricultor VARCHAR(45) DEFAULT 'Nenhum complemento.',
+  bairroAgricultor VARCHAR(65) NOT NULL,
+  cidadeAgricultor VARCHAR(45) NOT NULL,
+  estadoAgricultor VARCHAR(45) NOT NULL,
+  cepAgricultor CHAR(9) UNIQUE NOT NULL
 );
 
 CREATE TABLE tbFoneAgricultor (
   idfoneAgricultor INT PRIMARY KEY AUTO_INCREMENT,
-  numTelefoneAgricultor CHAR(13) NOT NULL,
+  numTelefoneAgricultor CHAR(11) NOT NULL,
   fkAgricultor INT,
-  CONSTRAINT fkAgric FOREIGN KEY (fkAgricultor) REFERENCES tbAgricultor (idAgricultor)
+  CONSTRAINT fkAgricultorFone FOREIGN KEY (fkAgricultor) REFERENCES tbAgricultor (idAgricultor)
 );
 
 CREATE TABLE tbCaminhao (
@@ -33,46 +36,45 @@ CREATE TABLE tbCaminhao (
   comprimentoCaminhao DECIMAL(5, 2) NOT NULL,
   alturaCaminhao DECIMAL(4, 2) NOT NULL,
   odometroCaminhao INT NOT NULL,
-  tipoCombustivelCaminhao VARCHAR(10) NOT NULL,
+  tipoCombustivelCaminhao VARCHAR(25) NOT NULL,
   CONSTRAINT chkTipoCombustivel CHECK (tipoCombustivelCaminhao IN ('Diesel Comum', 'Diesel S-10', 'Diesel aditivado', 'Diesel premium', 'Gás Natural', 'Eletricidade', 'Outro'))
 );
 
 CREATE TABLE tbProduto (
   idProduto INT PRIMARY KEY AUTO_INCREMENT,
-  tipoProduto VARCHAR(10) NOT NULL,
+  tipoProduto VARCHAR(15) NOT NULL,
   nomeProduto VARCHAR(80) NOT NULL,
   tempIdeal DECIMAL(5, 2) NOT NULL,
   umidIdeal DECIMAL(5, 2) NOT NULL,
   CONSTRAINT chkTipoProduto CHECK (tipoProduto IN ('Fruta')),
-  fkAgricultor INT,
-  CONSTRAINT fkAgric FOREIGN KEY (fkAgricultor) REFERENCES tbAgricultor (idAgricultor)
+  fkAgricultor INT, CONSTRAINT fkAgricultorProduto FOREIGN KEY (fkAgricultor) REFERENCES tbAgricultor(idAgricultor)
 ) AUTO_INCREMENT = 10;
 
 CREATE TABLE tbRemessa (
   idRemessa INT PRIMARY KEY AUTO_INCREMENT,
-  quantidade INT NOT NULL,
-  dataEnvio DATE NOT NULL,
-  dataRecebimento DATE NOT NULL,
+  quantidadeRemessa INT NOT NULL,
+  dataEnvioRemessa DATE NOT NULL,
+  dataRecebimentoRemessa DATE NOT NULL,
   LogradourolocalOrigem VARCHAR(80) NOT NULL,
   numLogradouroLO INT NOT NULL,
   complementoLO VARCHAR(40) NOT NULL,
   bairroLO VARCHAR(40) NOT NULL,
   cidadeLO VARCHAR(40) NOT NULL,
   estadoLO VARCHAR(40) NOT NULL,
-  cepLO CHAR(8) UNIQUE NOT NULL,
+  cepLO CHAR(9) UNIQUE NOT NULL,
   LogradourolocalDestino VARCHAR(80) NOT NULL,
   numLogradouroLD INT NOT NULL,
   complementoLD VARCHAR(40),
   bairroLD VARCHAR(40) NOT NULL,
   cidadeLD VARCHAR(40) NOT NULL,
   estadoLD VARCHAR(40) NOT NULL,
-  cepLD CHAR(8) UNIQUE NOT NULL,
-  NCM VARCHAR(10) NOT NULL,
-  distancia DOUBLE NOT NULL,
+  cepLD CHAR(9) UNIQUE NOT NULL,
+  NCMRemessa CHAR(10) NOT NULL,
+  distanciaRemessa DOUBLE NOT NULL,
   fkProduto INT,
-  CONSTRAINT fkProdut FOREIGN KEY (fkProduto) REFERENCES Produto (idProduto),
+  CONSTRAINT fkProdutoRemessa FOREIGN KEY (fkProduto) REFERENCES tbProduto(idProduto),
   fkCaminhao CHAR(7),
-  CONSTRAINT fkCaminh FOREIGN KEY (fkCaminhao) REFERENCES tbCaminhao (placaCaminhao)
+  CONSTRAINT fkCaminhaoRemessa FOREIGN KEY (fkCaminhao) REFERENCES tbCaminhao(placaCaminhao)
 ) AUTO_INCREMENT = 100;
 
 CREATE TABLE tbSensor (
@@ -82,14 +84,18 @@ CREATE TABLE tbSensor (
   CONSTRAINT chktipo CHECK (tipo IN ('DHT11', 'LM35')),
   CONSTRAINT chkStatusMonitoramento CHECK (statusMonitoramento IN ('Ativo', 'Inativo')),
   fkCaminhao CHAR(7),
-  CONSTRAINT fkCaminh FOREIGN KEY (fkCaminhao) REFERENCES tbCaminhao (placaCaminhao)
+  CONSTRAINT fkCaminhaoSensor FOREIGN KEY (fkCaminhao) REFERENCES tbCaminhao (placaCaminhao)
 );
+
 
 CREATE TABLE tbSensorDados (
   id_sensorDados INT PRIMARY KEY AUTO_INCREMENT,
   umidade DOUBLE NOT NULL,
   temperatura DOUBLE NOT NULL,
   data_Hora DATETIME DEFAULT CURRENT_TIMESTAMP,
-  fkSensor CHAR(7),
-  CONSTRAINT fkSensor FOREIGN KEY (fkSensor) REFERENCES tbSenor(idSensor)
+  fkSensor INT,
+  CONSTRAINT fkSensorSensorDados FOREIGN KEY (fkSensor) REFERENCES tbSensor(idSensor)
 );
+
+
+
---
+++ Código Fonte/Banco de Dados/InsertDatabase_Fruittech.sql
@@ -0,0 +1,5 @@
+USE FruitTech;
+
+INSERT INTO tbAgricultor VALUES
+	(NULL,'Eraí','Maggi','469.537.530-30','18.513.341/0001-26','reidasoja','21 UBAG', 'Av. dos Florais','nº 1.788','Proximo ao posto.','Bairro Ribeirão do Lipa','Cuiabá','Mato Grosso','78049-520'),
+	(NULL, 'José', 'Luís', '999.302.920-39', '08.293.543/0001-56', 'AgriTech564', 'OrangeClean', 'Estrada do Gopiuva', '233', 'Mercado Dois Irmãos', 'Parque Central Vidi', 'Carapicuíba', 'São Paulo','06395-530');
